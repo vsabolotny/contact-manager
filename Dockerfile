e3d369b1746c5724f4e8326657857028b40e27bf
Vladislav Sabolotny <vladislav.sabolotny@gmail.com>
2021-05-18 18:16:13 +0200

    Create redux application

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,10 @@ COPY package.json .
 
 RUN npm install
 
+RUN npm install redux 
+
+RUN npm install react-redux
+
 COPY . /app
 
 EXPOSE 3000
--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PeopleList from './components/PeopleList';
+import AddPersonForm from './components/AddPersonForm';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import './style.css';
+
+const initialState = {
+  contacts: ["James Smith", "Thomas Anderson", "Bruce Wayne"] 
+  };
+
+// Reducer function
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'ADD_PERSON':
+      return {...state,
+        contacts: [...state.contacts, action.data]}
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+ReactDOM.render(
+  <Provider store={store}>
+    <AddPersonForm />
+    <PeopleList />
+  </Provider>,
+  document.getElementById('root')
+);
+
+/** Contact Manager v1
+
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
@@ -73,7 +107,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-/**
+-- 
 
 function AddForm() {
   const [sum, setSum] = useState(0);
